41ec91ea13bb8ccc67b3e504db24862aafeb5fab
Nurzhan Donbaev <nurjan.donbaev@gmail.com>
2022-06-21 22:52:22 +0600

    added enum class

---
+++ src/com/company/Hand.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public enum Hand {
+    ROCK(1, "Rock"), PAPER(2, "Paper"), SCISSORS(3, "Scissors");
+
+    private int value;
+    private String name;
+
+    Hand(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
