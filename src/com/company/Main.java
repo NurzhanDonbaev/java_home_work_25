a9736b483382204ca76f201537c8891232684add
Nurzhan Donbaev <nurjan.donbaev@gmail.com>
2022-06-21 22:54:40 +0600

    added main game logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        getGame();
+    }
+
+    public static void getGame() {
+        Hand rock = Hand.ROCK;
+        Hand paper = Hand.PAPER;
+        Hand scissors = Hand.SCISSORS;
+        int userCount = 0;
+        int compCount = 0;
+        int drawCount = 0;
+        int percent = 0;
+        int gameCount = 0;
+        for (int i = 0; i < 10; i++) {
+            Random rn = new Random();
+            Scanner sc = new Scanner(System.in);
+            int userTurn = sc.nextInt();
+            int copmTurn = rn.nextInt(3) + 1;
+            gameCount++;
+            System.out.println("yuo");
+            if (userTurn == rock.getValue() && copmTurn == rock.getValue() || userTurn == paper.getValue() && copmTurn == paper.getValue() || userTurn == scissors.getValue() && copmTurn == scissors.getValue()) {
+                drawCount++;
+            }
+            if (userTurn == paper.getValue() && copmTurn == rock.getValue() || userTurn == scissors.getValue() && copmTurn == paper.getValue() || userTurn == rock.getValue() && copmTurn == scissors.getValue()) {
+                userCount++;
+            }
+            if (userTurn == scissors.getValue() && copmTurn == rock.getValue() || userTurn == paper.getValue() && copmTurn == scissors.getValue() || userTurn == rock.getValue() && copmTurn == paper.getValue()) {
+                compCount++;
+            }
+        }
+
     }
 }
