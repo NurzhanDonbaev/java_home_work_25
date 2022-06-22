29d0ec874ce908eeeccb6c671b6309e3fde7b62d
Nurzhan Donbaev <nurjan.donbaev@gmail.com>
2022-06-22 20:25:59 +0600

    added the stucture to working by user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main {
         getGame();
     }
 
+
     public static void getGame() {
         Hand rock = Hand.ROCK;
         Hand paper = Hand.PAPER;
@@ -18,13 +19,35 @@ public class Main {
         int drawCount = 0;
         int percent = 0;
         int gameCount = 0;
+        String format = "| %-7s | %-8s | %-9s | %15s | %9s |";
         for (int i = 0; i < 10; i++) {
             Random rn = new Random();
+            System.out.println("1 - Rock, 2 - Paper, 3 - Scissors");
+            System.out.print("Please select your turn: ");
             Scanner sc = new Scanner(System.in);
             int userTurn = sc.nextInt();
             int copmTurn = rn.nextInt(3) + 1;
+            System.out.print("Your turn is ");
+            if (userTurn == 1) {
+                System.out.println(rock.getName());
+            }
+            if (userTurn == 2) {
+                System.out.println(paper.getName());
+            }
+            if (userTurn == 3){
+                System.out.println(scissors.getName());
+            }
+            System.out.print("Computer selected:  ");
+            if (copmTurn == 1) {
+                System.out.println(rock.getName());
+            }
+            if (copmTurn == 2) {
+                System.out.println(paper.getName());
+            }
+            if (copmTurn == 3){
+                System.out.println(scissors.getName());
+            }
             gameCount++;
-            System.out.println("yuo");
             if (userTurn == rock.getValue() && copmTurn == rock.getValue() || userTurn == paper.getValue() && copmTurn == paper.getValue() || userTurn == scissors.getValue() && copmTurn == scissors.getValue()) {
                 drawCount++;
             }
@@ -34,7 +57,22 @@ public class Main {
             if (userTurn == scissors.getValue() && copmTurn == rock.getValue() || userTurn == paper.getValue() && copmTurn == scissors.getValue() || userTurn == rock.getValue() && copmTurn == paper.getValue()) {
                 compCount++;
             }
+            percent = ((userCount * 100) / gameCount);
+            getHeader1();
+            String view = String.format(format, userCount, compCount, drawCount, gameCount, percent + " %");
+            System.out.println(view);
+            getHeader2();
         }
 
+    }
+
+    public static void getHeader1() {
+        System.out.println("+--------------------------------------------------------------+");
+        System.out.println("|   Won   |   Lose   |   Draws   |   Total Games   |   Win %   |");
+        System.out.println("|--------------------------------------------------------------|");
+    }
+
+    public static void getHeader2() {
+        System.out.println("+--------------------------------------------------------------+");
     }
 }
